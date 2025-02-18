5abf2e8b0780202ad552ac729795e6d569b37063
DanteLuque <danteluque706@gmail.com>
2025-02-18 15:27:28 -0500

    implementando logica y mejorando interfaz de triangulo

--- app/src/main/java/com/senati/calculadora/Triangulo.java
+++ app/src/main/java/com/senati/calculadora/Triangulo.java
@@ -3,6 +3,7 @@ package com.senati.calculadora;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,19 +12,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+
 import java.text.DecimalFormat;
 
 public class Triangulo extends AppCompatActivity {
 
     private EditText tv_base, tv_altura;
     private TextView tv_resultado;
+    private ImageView img_triangulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangulo);
-
         loadUI();
+        cargarGIF();
     }
 
     // eventos
@@ -43,5 +47,12 @@ public class Triangulo extends AppCompatActivity {
         tv_base = (EditText) findViewById(R.id.tv_base);
         tv_altura = (EditText) findViewById(R.id.tv_altura);
         tv_resultado = (TextView) findViewById(R.id.tv_resultado);
+        img_triangulo = (ImageView) findViewById(R.id.img_triangulo);
+    }
+
+    public void cargarGIF(){
+        Glide.with(this)
+                .load(R.drawable.triangulo)
+                .into(img_triangulo);
     }
 }
