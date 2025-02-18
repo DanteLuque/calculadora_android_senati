14a5d7d2d45752c0b7653a06c01390e20a844ba2
DanteLuque <danteluque706@gmail.com>
2025-02-17 23:05:12 -0500

    mejorando la UI en activity_main

--- app/src/main/java/com/senati/calculadora/MainActivity.java
+++ app/src/main/java/com/senati/calculadora/MainActivity.java
@@ -3,6 +3,7 @@ package com.senati.calculadora;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +11,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView img_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        loadUI();
+        cargarGIF();
     }
 
     public void redirectMenu(View view){
         Intent intent = new Intent(getApplicationContext(), Menu.class);
         startActivity(intent);
+    }
+
+    public void loadUI(){
+         img_main = (ImageView) findViewById(R.id.img_main);
+    }
+
+    public void cargarGIF(){
+        Glide.with(this)
+                .load(R.drawable.calculadora)
+                .into(img_main);
     }
 }
